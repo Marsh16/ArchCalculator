eae5dbd950f9494c6f2fad95edaccc6e19e10da3
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2024-04-29 16:03:54 +0700

    Calculate View 75% Done

--- ArchCalculator/Views/CalculateView.swift
+++ ArchCalculator/Views/CalculateView.swift
@@ -25,39 +25,49 @@ struct CalculateView: View {
                     }
                 })
                 VStack{
-                    HStack{
-                        Text("Column Type")
-                        Picker("Column Type", selection: $category){
-                            Text("Practical Column").tag(0)
-                            Text("a Column").tag(1)
-                            Text("b Column").tag(2)
-                        }.frame(maxWidth: .infinity).multilineTextAlignment(.trailing)
-                    }
-                    Divider()
-                    HStack{
-                        Text("Length")
-                        TextField("Length", text: $length).multilineTextAlignment(.trailing)
-                    }
-                    Divider()
-                    HStack{
-                        Text("Width")
-                        TextField("Width", text: $width).multilineTextAlignment(.trailing)
-                    }
-                    Divider()
-                    Button("Calculate"){
+                    VStack{
+                        HStack{
+                            Text("Column Type")
+                            Spacer()
+                            Picker("Column Type", selection: $category){
+                                Text("Practical Column").tag(0)
+                                Text("a Column").tag(1)
+                                Text("b Column").tag(2)
+                            }.multilineTextAlignment(.trailing)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Length")
+                            TextField("Length", text: $length).multilineTextAlignment(.trailing)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Width")
+                            TextField("Width", text: $width).multilineTextAlignment(.trailing)
+                        }
+                        Divider()
                         
+                        Button("Calculate"){
+                            
+                        }.padding()
+                        HStack{
+                            Text("Steel Beams").fontWeight(.bold).font(.subheadline).foregroundColor(Colors.greyColor)
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                            Text("15 Pieces").fontWeight(.regular).font(.caption)
+                                .multilineTextAlignment(.trailing)
+                        }.padding().frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/).listRowSeparator(.hidden).background(Color.white).clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(color: Colors.blurColor, radius: 8).padding(.vertical,2)
+                        HStack{
+                            Text("Steel Beams").fontWeight(.bold).font(.subheadline).foregroundColor(Colors.greyColor)
+                                .frame(maxWidth: .infinity, alignment: .topLeading)
+                            Text("15 Pieces").fontWeight(.regular).font(.caption)
+                                .multilineTextAlignment(.trailing)
+                        }.padding().frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/).listRowSeparator(.hidden).background(Color.white).clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(color: Colors.blurColor, radius: 8).padding(.vertical,2)
+                        Image("ArchMountain").resizable().padding().frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/).listRowSeparator(.hidden).background(Color.white).clipShape(RoundedRectangle(cornerRadius: 10))
+                            .shadow(color: Colors.blurColor, radius: 8).padding(.vertical,2)
                     }.padding()
-                    
-                    HStack{
-                        Text("Length")
-                        TextField("Length", text: $length).multilineTextAlignment(.trailing)
-                    }
-                    HStack{
-                        Text("Width")
-                        TextField("Width", text: $width).multilineTextAlignment(.trailing)
-                    }
-                    Image("ArchMountain").resizable()
-                }.padding()
+                }
             }
         }
     }
