f5fbc156fc20d2108cc6bc8cb48328add60470f7
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2024-04-29 15:30:48 +0700

    Calculate View 50%

--- ArchCalculator/Views/CalculateView.swift
+++ ArchCalculator/Views/CalculateView.swift
@@ -11,6 +11,9 @@ struct CalculateView: View {
     @Environment (\.presentationMode) private var presentationMode: Binding<PresentationMode>
     @State var chosenStructure : StructurePlan
     @EnvironmentObject var calculateViewModel: CalculateViewModel
+    @State var category:Int = 0
+    @State var length:String = ""
+    @State var width:String = ""
     var body: some View {
         NavigationStack{
             VStack{
@@ -21,10 +24,40 @@ struct CalculateView: View {
                         })
                     }
                 })
-                List{
-                    Text("calculateViewModel.searchText")
-                }
-                Text("saasas")
+                VStack{
+                    HStack{
+                        Text("Column Type")
+                        Picker("Column Type", selection: $category){
+                            Text("Practical Column").tag(0)
+                            Text("a Column").tag(1)
+                            Text("b Column").tag(2)
+                        }.frame(maxWidth: .infinity).multilineTextAlignment(.trailing)
+                    }
+                    Divider()
+                    HStack{
+                        Text("Length")
+                        TextField("Length", text: $length).multilineTextAlignment(.trailing)
+                    }
+                    Divider()
+                    HStack{
+                        Text("Width")
+                        TextField("Width", text: $width).multilineTextAlignment(.trailing)
+                    }
+                    Divider()
+                    Button("Calculate"){
+                        
+                    }.padding()
+                    
+                    HStack{
+                        Text("Length")
+                        TextField("Length", text: $length).multilineTextAlignment(.trailing)
+                    }
+                    HStack{
+                        Text("Width")
+                        TextField("Width", text: $width).multilineTextAlignment(.trailing)
+                    }
+                    Image("ArchMountain").resizable()
+                }.padding()
             }
         }
     }
